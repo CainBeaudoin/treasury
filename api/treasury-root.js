243d6f00070fc1c32ae7c00bbdbe664c63a27f36
Cain <124520365+CainBeaudoin@users.noreply.github.com>
2026-09-12 14:20:29 +0400

    Load final Auto Credit Back UI fixes

--- api/treasury-root.js
+++ api/treasury-root.js
@@ -5,8 +5,15 @@ module.exports = function handler(req, res) {
   const htmlPath = path.join(process.cwd(), 'index.html');
   let html = fs.readFileSync(htmlPath, 'utf8');
 
+  const scripts = [
+    '  <script src="/policy-overrides.js"></script>',
+    '  <script src="/policy-fixes.js"></script>'
+  ];
+
   if (!html.includes('policy-overrides.js')) {
-    html = html.replace('</body>', '  <script src="/policy-overrides.js"></script>\n</body>');
+    html = html.replace('</body>', `${scripts.join('\n')}\n</body>`);
+  } else if (!html.includes('policy-fixes.js')) {
+    html = html.replace('</body>', `${scripts[1]}\n</body>`);
   }
 
   res.setHeader('Content-Type', 'text/html; charset=utf-8');
